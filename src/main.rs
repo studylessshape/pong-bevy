57dd693b2ff47f7dbb0c2f6d8559f8a5a219676b
studylessshape <a664554724@qq.com>
2024-11-04 11:25:17 +0700

    Creating a ball

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, spawn_ball)
+        .run();
+}
+
+fn spawn_ball(mut commands: Commands) {
+    commands.spawn_empty();
 }
