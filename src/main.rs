8067d6f69e5898745dd550d8cc39ac0e4d3fa527
studylessshape <a664554724@qq.com>
2024-11-04 12:08:13 +0700

    Creating a camera

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, spawn_ball)
+        .add_systems(Startup, (spawn_ball, spawn_camera))
         .run();
 }
 
 fn spawn_ball(mut commands: Commands) {
     commands.spawn_empty();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn_empty().insert(Camera2dBundle::default());
 }
